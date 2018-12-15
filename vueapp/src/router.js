b87404f06e9a6a87e0fe591fe9e185666897c643
jirawat070 <jirawat_pao@homail.co.th>
2018-12-15 15:46:36 +0700

    edit eror

--- vueapp/src/router.js
+++ vueapp/src/router.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue);
 
 Vue.use(Router)
 
